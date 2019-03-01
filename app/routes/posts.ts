7c9623e0ba5a2c3b2635789d8195090e85598464
Abiral Sthapit <abi.sthapit@gmail.com>
2019-03-01 23:14:25 +0545

    Passport functionality is added for protecting the routes that needs to be authorized

--- app/routes/posts.ts
+++ app/routes/posts.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import passport from "passport";
 import Post from "../controller/Post";
 
 const router: Router = Router();
@@ -7,6 +8,15 @@ const router: Router = Router();
 //@desc     Tests post route
 //@access   Public
 router.get("/test", (req, res) => res.json({ msg: "Post work" }));
+
+//@route    GET api/post/ptest
+//@desc     Tests protected post route
+//@access   Private
+router.get(
+  "/ptest",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => res.json({ msg: "Protected Post work" })
+);
 
 router.get("/create", Post.create);
 
