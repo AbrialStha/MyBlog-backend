5a4266505a31feee67414c83c356c8e3e8c93b00
Abiral Sthapit <abiral@fusemachines.com>
2019-07-16 23:28:58 +0545

    change the require to import for fs system

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import fs from 'fs'
 
 let routes: { [key: string]: Router } = {}
-require('fs').readdirSync(__dirname + '/').forEach(function (file: string) {
+fs.readdirSync(`${__dirname}/`).forEach(function (file: string) {
   var name = file.split('.')[0];
   if (!name.includes("index"))
     routes[name] = require(`./${name}`).default
